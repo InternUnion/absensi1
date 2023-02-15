606b330b7beed4d56f984f1ec64d4c94d8afb495
Rakha <rakhaallea@gmail.com>
2023-02-15 12:31:40 +0700

    Login desaign,tambah img
    By Rakha

---
+++ page/login.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>A R A K Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="MyraStudio" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="../img/logo.jpg">
+
+    <!-- App css -->
+    <link href="../template/Admin/vertical/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="../template/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="../template/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+ 
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="d-flex align-items-center min-vh-100">
+                        <div class="w-100 d-block bg-white shadow-lg rounded my-5">
+                            <div class="row">
+                                
+                                <img class="col-lg-5 d-none d-lg-block rounded-left" src="../img/logo.jpg" alt="">
+                                <div class="col-lg-7">
+                                    <div class="p-5">
+                                        <div class="text-center mb-5">
+                                            <a href="index.html" class="text-dark font-size-22 font-family-secondary">
+                                                <h1>LOGIN</h1>
+                                            </a>
+                                        </div>
+                                        <p class="text-muted mb-4">Enter your Username and password to access admin panel.</p>
+                                        <form class="user">
+                                            <div class="form-group">
+                                                <input type="text" class="form-control form-control-user" id="exampleInputEmail" placeholder="Username">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                                            </div>
+                                            <a href="#" class="btn btn-success btn-block waves-effect waves-light"> Log In </a>
+
+                                            
+                                            
+                                        </form>
+
+                                        <div class="row mt-4">
+                                            <div class="col-12 text-center">
+                                                
+                                                <p class="text-muted mb-0">Don't have an account? <a href="register.php" class="text-muted font-weight-medium ml-1"><b>Register</b></a></p>
+                                            </div> <!-- end col -->
+                                        </div>
+                                        <!-- end row -->
+                                    </div> <!-- end .padding-5 -->
+                                </div> <!-- end col -->
+                            </div> <!-- end row -->
+                        </div> <!-- end .w-100 -->
+                    </div> <!-- end .d-flex -->
+                </div> <!-- end col-->
+            </div> <!-- end row -->
+        </div>
+        <!-- end container -->
+    </div>
+    <!-- end page -->
+
+    <!-- jQuery  -->
+    <script src="../template/Admin/vertical/assets/js/jquery.min.js"></script>
+    <script src="../template/Admin/vertical/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="../template/Admin/vertical/assets/js/metismenu.min.js"></script>
+    <script src="../template/Admin/vertical/assets/js/waves.js"></script>
+    <script src="../template/Admin/vertical/assets/js/simplebar.min.js"></script>
+
+    <!-- App js -->
+    <script src="../template/Admin/vertical/assets/js/theme.js"></script>
+
+</body>
+
+</html>
