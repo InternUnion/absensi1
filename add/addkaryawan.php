9a9f66ee82ad2226b6110e4bdf2bddfdab50ad80
AdamMuafa <adammuafa28@gmail.com>
2023-02-15 16:12:25 +0700

    Create addkaryawan.php
    by adam

---
+++ add/addkaryawan.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Xeloro - Admin & Dashboard Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="MyraStudio" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+        <!-- App css -->
+        <link href="../template/Admin/horizontal/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="../template/Admin/horizontal/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="../template/Admin/horizontal/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+
+    </head>
+<body>
+<div class="card">
+                                    <div class="card-body">
+                                        <h4 class="card-title">Form Controls </h4>
+                                        <p class="card-subtitle mb-4">Textual form controls—like <code>input</code>s,<code> selects</code>, and<code> textare</code>s—are styled with the .form-control class. Included are styles for general appearance, focus state, sizing, and more. </p>
+    
+                                        <form>
+                                            <div class="form-group">
+                                                <label for="simpleinput">Text</label>
+                                                <input type="text" id="simpleinput" class="form-control" placeholder="Enter your text">
+                                            </div>
+    
+                                            <div class="form-group">
+                                                <label for="example-password">Password</label>
+                                                <input type="password" id="example-password" class="form-control" value="password">
+                                            </div>
+                                                    
+                                            <div class="form-group">
+                                                <label for="exampleFormControlInput1">Email address</label>
+                                                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="exampleFormControlSelect1">Select</label>
+                                                <select class="form-control" id="exampleFormControlSelect1">
+                                                    <option>1</option>
+                                                    <option>2</option>
+                                                    <option>3</option>
+                                                    <option>4</option>
+                                                    <option>5</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="exampleFormControlSelect2">Multiple Select</label>
+                                                <select multiple class="form-control" id="exampleFormControlSelect2">
+                                                    <option>1</option>
+                                                    <option>2</option>
+                                                    <option>3</option>
+                                                    <option>4</option>
+                                                    <option>5</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="exampleFormControlTextarea1">Textarea</label>
+                                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="formControlRange">Range input</label>
+                                                <input type="range" class="form-control-range" id="formControlRange">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="customRange3">Custom Range</label>
+                                                <input type="range" class="custom-range" min="0" max="5" step="0.5" id="customRange3">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>File Browser</label>
+                                                <div class="custom-file">
+                                                    <input type="file" class="custom-file-input" id="customFile">
+                                                    <label class="custom-file-label" for="customFile">Choose file</label>
+                                                </div>            
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="example-readonly">Readonly</label>
+                                                <input type="text" id="example-readonly" class="form-control" readonly="" value="Readonly value">
+                                            </div>
+                                            
+                                            <div class="form-group">
+                                                <label for="example-disable">Disabled</label>
+                                                <input type="text" class="form-control" id="example-disable" disabled="" value="Disabled value">
+                                            </div>
+                                            
+                                            <div class="form-group">
+                                                <label for="example-static">Static control</label>
+                                                <input type="text" readonly class="form-control-plaintext" id="example-static" value="email@example.com">
+                                            </div>
+                                            
+                                            <div class="form-group mb-0">
+                                                <label for="example-helping">Helping text</label>
+                                                <input type="text" id="example-helping" class="form-control" placeholder="Helping text">
+                                                <span class="help-block"><small>A block of help text that breaks onto a new line and may extend beyond one line.</small></span>
+                                            </div>
+                                            
+                                        </form>
+                                    </div>
+                                    <!-- end card-body-->
+                                </div>
+<!-- jQuery  -->
+<script src="assets/js/jquery.min.js"></script>
+        <script src="../template/Admin/horizontal/assets/js/bootstrap.bundle.min.js"></script>
+        <script src="../template/Admin/horizontal/assets/js/waves.js"></script>
+        <script src="../template/Admin/horizontal/assets/js/simplebar.min.js"></script>
+
+        <!-- App js -->
+        <script src="../template/Admin/horizontal/assets/js/theme.js"></script>
+
+    </body>
+
+</html>
