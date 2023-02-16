1d09154806e1cdd1056b5409214ee13ce0ab6c27
Rakha <rakhaallea@gmail.com>
2023-02-16 16:07:58 +0700

    Update addjabatan.php
    by rakha

--- add/addjabatan.php
+++ add/addjabatan.php
@@ -162,12 +162,12 @@
                         <div class="row">
                             <div class="col-12">
                                 <div class="page-title-box d-flex align-items-center justify-content-between">
-                                    <h4 class="mb-0 font-size-100">Data Users</h4>
+                                    <h4 class="mb-0 font-size-100">Data Position (masih eror di bagian edit)</h4>
     
                                     <div class="page-title-right">
                                         <ol class="breadcrumb m-0">
                                             <li class="breadcrumb-item"><a href="javascript: void(0);">ARAK</a></li>
-                                            <li class="breadcrumb-item active">Data Users</li>
+                                            <li class="breadcrumb-item active">Data Position</li>
                                         </ol>
                                     </div>
                                 </div>
