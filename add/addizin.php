507794cc644769f58b985d8f8b65fc3308e0d126
AdamMuafa <adammuafa28@gmail.com>
2023-02-21 15:52:21 +0700

    update addizin.php
    by adam

--- add/addizin.php
+++ add/addizin.php
@@ -113,12 +113,12 @@
                         <div class="row">
                             <div class="col-12">
                                 <div class="page-title-box d-flex align-items-center justify-content-between">
-                                    <h4 class="mb-0 font-size-100">Clock In</h4>
+                                    <h4 class="mb-0 font-size-100">Permision</h4>
     
                                     <div class="page-title-right">
                                         <ol class="breadcrumb m-0">
                                             <li class="breadcrumb-item"><a href="javascript: void(0);">ARAK</a></li>
-                                            <li class="breadcrumb-item active">Absent</li>
+                                            <li class="breadcrumb-item active">Permision</li>
                                         </ol>
                                     </div>
                                 </div>
@@ -143,8 +143,8 @@
 
                                 <div class="card">
                                     <div class="card-body">
-                                        <h4 class="card-title">Clock In</h4>
-                                        <p class="card-subtitle mb-4">Absent please !!</p>
+                                        <h4 class="card-title">Permision</h4>
+                                        <p class="card-subtitle mb-4">Insert Your Permision</p>
     
                                         <form method="POST" action="addizin.php">
                                         <div class="form-group" method="POST">
