38510abd6f0c9ca5a4888068f8e54694ed7a0f73
AdamMuafa <adammuafa28@gmail.com>
2023-03-01 17:14:17 +0700

    update connection report
    by adam

--- page/report.php
+++ page/report.php
@@ -178,7 +178,6 @@ $level = $_SESSION['level'];
                                                     <td><?php echo $dataabsen['clock_in']; ?></td>
                                                     <td><?php echo $dataabsen['clock_out']; ?></td>
                                                     <td><a href="../hapus/hapusabsen.php?id_absen=<?php echo $dataabsen['id_absen'];?>" class="btn btn-danger" ><i class="fas fa-trash"></i></a>
-                                                    <a href="clockout.php?id_absen=<?php echo $dataabsen['id_absen'];?>" class="btn btn-warning" ><i class="fas fa-clock"></i></a></center></td></td>
                                                 </tr>
                                 </div>
                             </div>
