099a8d5f5856af0149a3558c7819f605616ed463
Rakha <rakhaallea@gmail.com>
2023-03-01 12:27:49 +0700

    update conn
    by rakha

--- configure/connection.php
+++ configure/connection.php
@@ -1,8 +1,8 @@
 <?php
-    define('DB_SERVER','mysql-111283-0.cloudclusters.net:19688');
-    define('DB_USERNAME','admin');
-    define('DB_PASSWORD','fa9DTYBf');
-    define('DB_DATABASE','db_absensi');
+    define('DB_SERVER','127.0.0.1:3307');//127.0.0.1:3307 mysql-111283-0.cloudclusters.net:19688
+    define('DB_USERNAME','root');
+    define('DB_PASSWORD','');
+    define('DB_DATABASE','db_absensi1');
 
     $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
